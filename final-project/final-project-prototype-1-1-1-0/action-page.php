2113db440fe64c625e51f1036163a53905158649
t4r3q <47785561+t4r3q@users.noreply.github.com>
2019-08-15 11:07:42 +0200

    outline upload

---
+++ final-project/final-project-prototype-1-1-1-0/action-page.php
@@ -0,0 +1,20 @@
+<?php
+$nameErr = $emailErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+
+ 
+}
+?>
